0515bf3eafcc6d24d7e0eb4a3ff0db5b6f06aa6c
mcrwayfun <mcrwayfun@gamil.com>
2018-05-08 19:57:33 +0800

    修改.gitignore

--- spring-boot-helloworld/src/main/java/com/qingtian/controller/HomeController.java
+++ spring-boot-helloworld/src/main/java/com/qingtian/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @RequestMapping("/helloWorld")
+    @RequestMapping("/helloworld")
     public String sayHelloWorld(){
         return "hello world!";
     }
